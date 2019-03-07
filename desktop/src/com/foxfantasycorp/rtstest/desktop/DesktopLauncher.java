f8c74b02a84dd56a56e5bb9a8333c2563f69b301
jackCat13 <christophehenry11@gmail.com>
2019-03-07 23:21:06 +0100

    init commit with generated libGDX project structure

---
+++ desktop/src/com/foxfantasycorp/rtstest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.foxfantasycorp.rtstest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.foxfantasycorp.rtstest.RTSTestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RTSTestGame(), config);
+	}
+}
